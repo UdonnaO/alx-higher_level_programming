ceb4e868b2e441fd6d97fed61b94986e1df6eb54
Udonna Okebata <udonnaokebata@gmail.com>
2023-03-08 17:18:11 +0000

    third commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
-#!/usr/bin/phython3
+#!/usr/bin/python3
 def uppercase(str):
-    for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            c = chr(ord(c) - 32)
-        print(f"{c}", end="")
-    print("")
+    for ch in str:
+        if ord(ch) >= 97 and ord(ch) <= 122:
+            ch = chr(ord(ch) - 32)
+        print("{:s}".format(ch), end='')
+
+    print('\n', end="")
